5b64d48c5658b9e705d26c4d17fc7dc6247788eb
alimc <Alimchik.228@mail.ru>
2024-12-01 23:43:45 +0300

    Replace NER

--- moduls/ner_natasha.py
+++ moduls/ner_natasha.py
@@ -1,3 +1,5 @@
+import pymorphy2
+
 from natasha import (
     Segmenter,
     NewsEmbedding,
@@ -6,6 +8,7 @@ from natasha import (
     NewsNERTagger,
     Doc
 )
+import data.names
 class NER_NATASHA:
     def __init__(self):
         self.segmenter = Segmenter()
@@ -21,4 +24,58 @@ class NER_NATASHA:
         doc.parse_syntax(self.syntax_parser)
         doc.tag_ner(self.ner_tagger)
         return doc.spans
+
+    def replace_words(self, text, spans):
+        morph = pymorphy2.MorphAnalyzer()
+
+        dict_spans = {}
+        spans_type_dict = {
+            'female_names': open('../data/names/female_names_rus.txt', 'r', encoding='utf-8').read().split(sep='\n'),
+            'male_names': open('../data/names/male_names_rus.txt', 'r', encoding='utf-8').read().split(sep='\n'),
+            'male_surnames_names': open('../data/names/male_surnames_rus.txt', 'r', encoding='utf-8').read().split(sep='\n'),
+            'company': open('../data/names/company.txt', 'r', encoding='utf-8').read().split(sep='\n'),
+            'locations': open('../data/names/location.txt', 'r', encoding='utf-8').read().split(sep='\n'),
+        }
+
+        female_names_cnt = 0
+        male_names_cnt = 0
+        male_surnames_names_cnt = 0
+        company_cnt = 0
+        locations_cnt = 0
+
+        for span in spans:
+            if span.text not in dict_spans:
+
+                if span.type == 'PER':
+                    if len(span.text.split()) >= 2:
+                        dict_spans[span.text] = spans_type_dict['male_names'][male_names_cnt] + ' ' + spans_type_dict['male_surnames_names'][male_names_cnt]
+                        male_names_cnt += 1
+                        male_surnames_names_cnt += 1
+                        continue
+
+                    parsed_word = morph.parse(span.text)[0]
+
+                    if parsed_word.tag.gender == 'femn':
+                        dict_spans[span.text] = spans_type_dict['female_names'][female_names_cnt]
+                        female_names_cnt += 1
+                        continue
+
+                    if parsed_word.tag.gender == 'masc':
+                        dict_spans[span.text] = spans_type_dict['male_names'][male_names_cnt]
+                        male_names_cnt += 1
+                        continue
+
+                if span.type == 'ORG ':
+                    dict_spans[span.text] = spans_type_dict['company'][company_cnt]
+                    company_cnt += 1
+                    continue
+
+                if span.type == 'LOC':
+                    dict_spans[span.text] = spans_type_dict['locations'][locations_cnt]
+                    locations_cnt += 1
+                    continue
+        for old_word, new_word in dict_spans.items():
+            text = text.replace(old_word, new_word)
+
+        return text
 
--- test_moduls/test_ner_natasha.py
+++ test_moduls/test_ner_natasha.py
@@ -1,7 +1,11 @@
 from moduls.ner_natasha import NER_NATASHA
 
 object_ner = NER_NATASHA()
-text = "Москва — столица России, а Владимир Путин — президент страны. номер +79174520180, паспорт: 8676 345676"
+text = open('../data/test_chat/chat_example.txt', 'r', encoding='utf-8').read()
+
 doc_spans = object_ner.extract_ner(text)
 for span in doc_spans:
     print(f"Сущность: {span.text}, Тип: {span.type}")
+
+zamena = object_ner.replace_words(text, doc_spans)
+print(zamena)
